44cee8e3fb4c893e9131622293d4e3b69d6fd3fe
Huthman King <kinghuthman@gmail.com>
2021-11-20 11:01:13 -0600

    home page: under construction

---
+++ king-huthman/src/pages/Homepage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import eyeGif from "../assets/gifs/Eye-Animation_Final_WhiteBG copy.gif";
+import "./homepage.styles.css";
+
+function HomePage() {
+  return (
+    <div className="homeContainer">
+      <h1>UNDER CONSTRUCTION</h1>
+      <h2>Looking to ask a question or work together?</h2>
+      <h3>Email me at kinghuthman@gmail.com</h3>
+      <img className="eye" src={eyeGif} alt="loading..." />
+    </div>
+  );
+}
+
+export default HomePage;
