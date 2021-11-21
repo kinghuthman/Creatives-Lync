85e1fec14661146d64ea12fcaed720a415d6623a
Huthman King <kinghuthman@gmail.com>
2021-11-20 21:05:50 -0600

    woaaah dude

--- king-huthman/src/pages/Homepage.tsx
+++ king-huthman/src/pages/Homepage.tsx
@@ -5,7 +5,7 @@ import "./homepage.styles.css";
 function HomePage() {
   return (
     <div className="homeContainer">
-      <h1>UNDER CONSTRUCTION</h1>
+      <h1>Morgan is Bae</h1>
       <h2>Looking to ask a question or work together?</h2>
       <h3>Email me at kinghuthman@gmail.com</h3>
       <img className="eye" src={eyeGif} alt="loading..." />
